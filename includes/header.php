f21bd6f10f0a6130d666d09609afd2f5166a69a9
Becky Evensen <bree.brightone@gmail.com>
2018-02-02 11:10:14 -0600

    fixed header and footer

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,7 @@
 <header class="siteHeader">
-    <h1 class="siteHeader--title">Bree Brightone's Web</h1>
+    <div class="siteHeader--bar">
+        <div class="siteHeader--bar--inner">
+            <h1 class="siteHeader--bar--title">Bree Brightone's Web</h1>
+        </div>
+    </div>
 </header>
